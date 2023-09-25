f77fb5c1927a090c7e86a2869684664bb1f70784
Datck <112412957+DacCute@users.noreply.github.com>
2023-09-26 05:00:09 +0700

    Add files via upload

---
+++ AddAccessHistory.py
@@ -0,0 +1,8 @@
+import datetime
+import pandas as pd
+import numpy as np
+
+a = 'awdawdaw'
+np.savetxt(r'D:\Bill\np.txt', a, fmt='%s')
+
+ 
---
+++ Money.py
@@ -0,0 +1,69 @@
+import numpy as np
+import pandas as pd
+import datetime
+import sys
+from openpyxl import *
+import subprocess
+
+# &Instruction function
+def Instruction():
+    print("""
+--------------------------------------------
+Type 1 to add Transaction
+     2 to
+     3 to 
+     End to finish.
+--------------------------------------------
+          """)
+
+# ?Input task function
+def InputTask():
+    task = str(input('Telling what to do: '))
+    task = task.upper()
+
+# ^Get Time
+def GetTime():
+    current_time = datetime.datetime.now()
+
+# ^Add transaction funtion
+def AddTrans():
+    print('dob')
+
+def AddAccessHistory():
+    subprocess.run(["python", "D:\Bill\Calculation\AddAccessHistory.py"])
+
+# ^file address
+file_path = "D:\Bill\Calculation\Data.xlsx"
+
+# ^import file
+tran = pd.read_excel(file_path,
+                     sheet_name=['Transaction'])
+
+# ^Time access
+current_time = datetime.datetime.now()
+AddAccessHistory
+
+# ^Hello script
+print('''WELLCOME TO CALCULATION TRANSACTION SYSTEMS!
+--------------------------------------------
+Current time is:''',current_time,
+'\n--------------------------------------------')
+
+# !Program work here!
+Instruction()
+count = 0
+while True:
+    #task = str(input('Telling what to do: '))
+    #task = task.upper()
+    task = 'END'
+    if task == 'END':
+        break
+    else:
+        count +=1
+
+GetTime
+print('--------------------------------------------',
+      '\nEND TASK AT:  ',current_time,
+      '\n\n  ', count, 'task have done\n',
+      '\n--------------------------------------------'
+      )
